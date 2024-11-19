93a7fe078e4d1a7af0bd34669d6a4dd3f25d2c1a
vuphuong04 <lvphuonga23033@cusc.ctu.ctu.edu.vn>
2024-11-19 08:26:04 +0700

    second commit

--- config/database.config.js
+++ config/database.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/users');
+        await mongoose.connect('mongodb+srv://lvphuonga23033:<db_password>@cluster0.4scby.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log('Kết nối MongoDB thành công');
     } catch (err) {
         console.error('Lỗi kết nối MongoDB:', err);
